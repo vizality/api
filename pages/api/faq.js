d49b94572ec7202a39f616f15e169b34f6d16eac
dperolio <dustin@dperolio.com>
2023-01-16 12:16:58 -0500

    or just change the number

--- pages/api/faq.js
+++ pages/api/faq.js
@@ -138,11 +138,7 @@ export default async function handler (req, res) {
       }
     }
 
-    if (output.length) {
-      return res.status(200).json({ output });
-    }
-
-    return res.status(500).json(output);
+    return res.status(200).json(output);
   } catch (err) {
     return res.status(500).json({
       message: 'Server Error'
